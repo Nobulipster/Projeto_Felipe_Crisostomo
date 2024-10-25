076aad32dd68427a5cd5414beeefd20793d61ff7
Felipe Crisostomo <felipecrisostomo_@hotmail.com>
2024-10-24 21:28:09 -0300

    Adicionado Classe A e B

---
+++ A.py
@@ -0,0 +1,25 @@
+class A:
+    def __init__(self, A1: int, A2: float):
+        self._A1 = A1
+        self._A2 = A2
+
+    # Getters
+    def get_A1(self) -> int:
+        return self._A1
+
+    def get_A2(self) -> float:
+        return self._A2
+
+    # Setters
+    def set_A1(self, A1: int):
+        self._A1 = A1
+
+    def set_A2(self, A2: float):
+        self._A2 = A2
+
+    # Métodos
+    def MA1(self):
+        print("MA1")
+
+    def MA2(self):
+        print("MA2")
---
+++ B.py
@@ -0,0 +1,25 @@
+class B:
+    def __init__(self, B1: int, B2: float):
+        self._B1 = B1
+        self._B2 = B2
+
+    # Getters
+    def get_B1(self) -> int:
+        return self._B1
+
+    def get_B2(self) -> float:
+        return self._B2
+
+    # Setters
+    def set_B1(self, B1: int):
+        self._B1 = B1
+
+    def set_B2(self, B2: float):
+        self._B2 = B2
+
+    # Métodos
+    def MB1(self):
+        print("MB1")
+
+    def MB2(self):
+        print("MB2")
